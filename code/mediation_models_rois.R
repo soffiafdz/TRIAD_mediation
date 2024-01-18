e1aac9b8618c507dd17fe1da790d8c9f66339a98
sfernandez <sofia.fernandezlozano@mail.mcgill.ca>
2024-01-18 16:21:35 -0500

    Clean-up and semTables

--- code/mediation_models_rois.R
+++ code/mediation_models_rois.R
@@ -6,20 +6,21 @@ library(readr)
 library(stringr)
 library(lavaan)
 library(lavaanPlot)
+library(semTable)
 
 ## Refit models
 #refit_models <- TRUE
-refit_models <- TRUE
+refit_models  <- TRUE
 
 ## Print plots ### Needs to be done outside renv
-print_plots <- FALSE
+print_plots   <- FALSE
 #print_plots <- TRUE
 
 ## INPUT
 # Load baseline data
-fpath       <- here("data/rds/triad.rds")
+fpath         <- here("data/rds/triad.rds")
 if (file.exists(fpath)) {
-  triad.dt  <- read_rds(fpath)
+  triad.dt    <- read_rds(fpath)
 } else {
   here("code/demographics.R") |> source()
 }
@@ -27,7 +28,7 @@ rm(fpath)
 
 # Load Cerebra data
 # Biomarkers
-fpath       <- here("data/pet_biomarkers_cerebra.csv")
+fpath         <- here("data/pet_biomarkers_cerebra.csv")
 if (file.exists(fpath)) {
   cerebra.dt  <- fread(fpath)
 } else {
@@ -35,13 +36,13 @@ if (file.exists(fpath)) {
 }
 
 # ROIs
-fpaths      <- here("data/rds",
-                    sprintf("cerebra_rois_%s_moca.rds", c("amy", "tau")))
+fpaths        <- here("data/rds",
+                      sprintf("cerebra_rois_%s_moca.rds", c("amy", "tau")))
 if (any(!file.exists(fpaths))) {
   here("code/feature_selection.R") |> source()
 } else {
-  rois_amy  <- read_rds(fpaths[1])
-  rois_tau  <- read_rds(fpaths[2])
+  rois_amy    <- read_rds(fpaths[1])
+  rois_tau    <- read_rds(fpaths[2])
 }
 rm(fpaths)
 
@@ -49,152 +50,200 @@ rm(fpaths)
 # Convert Sex to dummy variable
 triad.dt[, SEX_n := as.numeric(SEX) - 1]
 
-# Remove youth and other dementias
-triad.dt    <- triad.dt[!DX_clean %in% c("Young", "Other", "AD")]
-#triad.dt    <- triad.dt[!DX_clean %in% c("Young", "Other")]
+# Remove youth and dementias
+triad.dt      <- triad.dt[!DX_clean %in% c("Young", "Other", "AD")]
 
 # 1 - HVR (average for both sides)
 #triad.dt[, `:=`(HVR_lr = 1 - HVR_l, HVR_rr = 1 - HVR_r)]
 triad.dt[, `:=`(HVR_mean_inv = 1 - (HVR_l + HVR_r) / 2)]
 
-triad.dt    <- triad.dt[, .(PTID, VISIT, DX, SEX_n, AGE_scan, EDUC,#APOE_n,
-                            HVR_mean_inv, MOCA_score)]
+triad.dt      <- triad.dt[, .(PTID, VISIT, DX, SEX_n, AGE_scan, EDUC,#APOE_n,
+                              HVR_mean_inv, MOCA_score)]
 
 # Merge triad and cerebra data
 # Cerebra dictionary
-dict_roi    <- unique(cerebra.dt[, .(LABEL_id, LABEL_name, SIDE)])
+dict_roi      <- unique(cerebra.dt[, .(LABEL_id, LABEL_name, SIDE)])
 
-# Amyloid
-amy.dt      <- rois_amy.dt[!is.na(cluster),
-                           .(LABEL_id = as.numeric(str_extract(id, "\\d{3}")),
-                             CLUSTER  = sprintf("AMY_%i", cluster))
-                           ][cerebra.dt, on = "LABEL_id"
-                           ][!is.na(AMYLOID) & !is.na(CLUSTER),
-                           weighted.mean(AMYLOID, VOL),
-                           .(PTID, VISIT, CLUSTER)] |>
-  dcast(... ~ CLUSTER, value.var = "V1")
+# Amyloid weighted average of all regions
+amy.dt        <- cerebra.dt[!is.na(AMYLOID),
+                            .(AMYLOID = weighted.mean(AMYLOID, VOL)),
+                            .(PTID, VISIT)]
+amy.dt        <- amy.dt[triad.dt, on = .(PTID, VISIT)]
 
-amy.dt  <- amy.dt[triad.dt, on = .(PTID, VISIT)]
+# Tau weighted average of all regions
+tau.dt        <- cerebra.dt[!is.na(TAU),
+                            .(TAU = weighted.mean(TAU, VOL)),
+                            .(PTID, VISIT)]
 
-# Tau
-tau.dt      <- rois_tau.dt[!is.na(cluster),
-                           .(LABEL_id = as.numeric(str_extract(id, "\\d{3}")),
-                             CLUSTER  = sprintf("TAU_%i", cluster))
-                           ][cerebra.dt, on = "LABEL_id"
-                           ][!is.na(TAU) & !is.na(CLUSTER),
-                           weighted.mean(TAU, VOL),
-                           .(PTID, VISIT, CLUSTER)] |>
-  dcast(... ~ CLUSTER, value.var = "V1")
-
-triad.dt  <- tau.dt[amy.dt, on = .(PTID, VISIT)]
-triad.dt  <- triad.dt[!is.na(AMY_1) & !is.na(TAU_1) & !is.na(MOCA_score)]
+triad_all.dt  <- tau.dt[amy.dt, on = .(PTID, VISIT)
+                        ][!is.na(MOCA_score) & !is.na(AMYLOID) & !is.na(TAU)]
 rm(amy.dt, tau.dt)
 
-### Labels
-#labels_cov  <- c(AGE_scan = "Age", SEX_n = "Sex", APOE_n = "APOE4")
-#labels_hcv  <- c(HCv_l = "Left", HCv_r = "Right")
-#labels_hvr  <- c(HVR = "HC-atrophy",
-                 #HVR_lr = "1-HVR (Left)",
-                 #HVR_rr = "1-HVR (Right)")
-#labels_hvr2 <- c(HVR_mean_inv = "HC-atrophy")
-#labels_moca <- c(MOCA_score = "MoCA")
+# Amyloid weighted average of selected features
+#amy.dt      <- rois_amy.dt[!is.na(cluster),
+                           #.(LABEL_id = as.numeric(str_extract(id, "\\d{3}")),
+                             #CLUSTER  = sprintf("AMY_%i", cluster))
+                           #][cerebra.dt, on = "LABEL_id"
+                           #][!is.na(AMYLOID) & !is.na(CLUSTER),
+                           #weighted.mean(AMYLOID, VOL),
+                           #.(PTID, VISIT, CLUSTER)] |>
+  #dcast(... ~ CLUSTER, value.var = "V1")
+amy.dt        <-
+  cerebra.dt[rois_amy.dt[decision == "Confirmed",
+                         .(LABEL_id = as.numeric(str_extract(id, "\\d{3}")))],
+             on = "LABEL_id"
+             ][!is.na(AMYLOID),
+               .(AMYLOID = weighted.mean(AMYLOID, VOL)),
+               .(PTID, VISIT)]
 
-### Models with latent variables ###
-amy_features  <- paste("AMY", 1:5, sep = "_")
-tau_features  <- paste("TAU", 1:5, sep = "_")
+amy.dt        <- amy.dt[triad.dt, on = .(PTID, VISIT)]
 
-latent.mod <-
+# Tau weighted average of selected features
+#tau.dt      <- rois_tau.dt[!is.na(cluster),
+                           #.(LABEL_id = as.numeric(str_extract(id, "\\d{3}")),
+                             #CLUSTER  = sprintf("TAU_%i", cluster))
+                           #][cerebra.dt, on = "LABEL_id"
+                           #][!is.na(TAU) & !is.na(CLUSTER),
+                           #weighted.mean(TAU, VOL),
+                           #.(PTID, VISIT, CLUSTER)] |>
+  #dcast(... ~ CLUSTER, value.var = "V1")
+tau.dt        <-
+  cerebra.dt[rois_tau.dt[decision == "Confirmed",
+                         .(LABEL_id = as.numeric(str_extract(id, "\\d{3}")))],
+             on = "LABEL_id"
+             ][!is.na(TAU),
+               .(TAU = weighted.mean(TAU, VOL)),
+               .(PTID, VISIT)]
+
+triad_sel.dt  <- tau.dt[amy.dt, on = .(PTID, VISIT)
+                        ][!is.na(MOCA_score) & !is.na(AMYLOID) & !is.na(TAU)]
+#triad.dt  <- triad.dt[!is.na(AMY_1) & !is.na(TAU_1) & !is.na(MOCA_score)]
+rm(amy.dt, tau.dt)
+
+## Simple model with selected ROIs
+## Model definitions
+hvr.mod       <-
   str_glue("
-           # Latent variables
-           AMYLOID =~ {paste(amy_features, collapse = ' + ')}
-           TAU =~ {paste(tau_features, collapse = ' + ')}
+           # Regressions
+           AMYLOID ~ SEX_n + AGE_scan
+           TAU ~ a * AMYLOID + SEX_n + AGE_scan
+           HVR_mean_inv ~ b * AMYLOID + c * TAU + SEX_n + AGE_scan
+           # HVR mediation
+           # Direct effect
+           dAMY  := b
+           # Indirect effect
+           iTAU  := a * c
+           # Total effect
+           Total := dAMY + iTAU
+           # Proportion analysis
+           propAMY := dAMY / Total
+           propTAU := iTAU / Total
+           ")
+
+moca.mod <-
+  str_glue("
            # Regressions
            AMYLOID ~ SEX_n + AGE_scan
            TAU ~ a * AMYLOID + SEX_n + AGE_scan
            HVR_mean_inv ~ b * AMYLOID + c * TAU + SEX_n + AGE_scan
            MOCA_score ~ d * AMYLOID + e * TAU + f * HVR_mean_inv + SEX_n + AGE_scan + EDUC
-           # HVR mediation
            # Direct effect
-           dAMY_HVR  := b
-           # Indirect effect
-           iTAU_HVR  := a * c
-           # Total effect
-           Total_HVR := dAMY_HVR + iTAU_HVR
-           # MOCA mediation
-           # Direct effect
-           dAMY_MOCA := d
+           dAMY := d
            # Indirect effects
-           iTAU_MOCA := a * e
-           iHVR_MOCA := Total_HVR * f
+           iTAU := a * e
+           iHVR := (b + (a * c)) * f
            # Total effects
-           iTotal    := iTAU_MOCA + iHVR_MOCA
-           Total_MOCA := iTotal + dAMY_MOCA
+           iTotal    := iTAU + iHVR
+           Total := iTotal + dAMY
            # Proportion analysis
-           propAMY_HVR := dAMY_HVR / Total_HVR
-           propTAU_HVR := iTAU_HVR / Total_HVR
-           propAMY_MOCA := dAMY_MOCA / Total_MOCA
-           propTAU_MOCA := iTAU_MOCA / Total_MOCA
-           propHVR_MOCA := iHVR_MOCA / Total_MOCA
-           propIndirect_MOCA := iTotal / Total_MOCA
+           propAMY := dAMY / Total
+           propTAU := iTAU / Total
+           propHVR := iHVR / Total
+           propInd := iTotal / Total
            ")
 
-fname <- here("data/rds/mediation_rois_latent.rds")
+## Simplest model with ALL ROIs
+# HVR mediation
+fname <- here("data/rds/mediation_simplest_hvr.rds")
 if (!file.exists(fname) | refit_models) {
-  model_rois_latent.fit <- sem(latent.mod,
-                               data = triad.dt[order(DX)],
-                               #group = "DX",
-                               estimator = "ML")
+  model_simplest_hvr.fit <- sem(hvr.mod,
+                                data = triad_all.dt,
+                                #group = "DX",
+                                estimator = "ML")
                      #se = "bootstrap", bootstrap = 10000)
-  write_rds(model_rois_latent.fit, fname)
+  write_rds(model_simplest_hvr.fit, fname)
+  semTable(model_simplest_hvr.fit, print.results = FALSE,
+           paramSets  = c("slopes", "constructed", "fits"),
+           columns    = c("estsestars", "z"),
+           fits       = c("npar", "chisq", "pvalue", "cfi", "tli",
+                          "aic", "bic2", "rmsea"),
+           type       = "html",
+           file       = here("data/derivatives/mediation_simplest_hvr.html"))
 } else {
-  model_rois_latent.fit <- read_rds(fname)
+  model_simplest_hvr.fit <- read_rds(fname)
 }
 rm(fname)
 
-### Model with all mediators ###
-# Text parsing
-amy_covars    <- paste(amy_features, collapse = " + ")
-tau_covars    <- paste(tau_features, collapse = " + ")
-
-amy_regress   <- sprintf("%s ~ SEX_n + AGE_scan", amy_features)
-tau_regress   <- sprintf("%s ~ %s + SEX_n + AGE_scan",
-                         tau_features, amy_covars)
-
-# Model definition
-detailed.mod <-
-  str_glue("
-## Regressions ##
-# Amyloid
-{paste(amy_regress, collapse = '\n')}
-# Tau
-{paste(tau_regress, collapse = '\n')}
-HVR_mean_inv ~ {amy_covars} + {tau_covars} + SEX_n + AGE_scan
-MOCA_score ~ {amy_covars} + {tau_covars} + HVR_mean_inv + SEX_n + AGE_scan + EDUC
-")
-
-fname <- here("data/rds/mediation_rois_detailed.rds")
+# MoCA mediation
+fname <- here("data/rds/mediation_rois_simplest_moca.rds")
 if (!file.exists(fname) | refit_models) {
-  model_rois_detailed.fit <- sem(detailed.mod,
-                                 data = triad.dt[order(DX)],
+  model_simplest_moca.fit <- sem(moca.mod,
+                                 data = triad_all.dt,
                                  #group = "DX",
                                  estimator = "ML")
                      #se = "bootstrap", bootstrap = 10000)
-  write_rds(model_rois_detailed.fit, fname)
+  write_rds(model_simplest_moca.fit, fname)
+  semTable(model_simplest_moca.fit, print.results = FALSE,
+           paramSets  = c("slopes", "constructed", "fits"),
+           columns    = c("estsestars", "z"),
+           fits       = c("npar", "chisq", "pvalue", "cfi", "tli",
+                          "aic", "bic2", "rmsea"),
+           type       = "html",
+           file       = here("data/derivatives/mediation_simplest_moca.html"))
 } else {
-  model_rois_detailed.fit <- read_rds(fname)
+  model_simplest_moca.fit <- read_rds(fname)
 }
 rm(fname)
 
-# Estimates data.table
-estimates.dt  <- parameterEstimates(model_rois_detailed.fit,
-                                    standardized = TRUE) |>
-           as.data.table()
+## Simple model with selected ROIs
+# HVR mediation
+fname <- here("data/rds/mediation_simple_hvr.rds")
+if (!file.exists(fname) | refit_models) {
+  model_simple_hvr.fit <- sem(hvr.mod,
+                               data = triad_sel.dt,
+                               #group = "DX",
+                               estimator = "ML")
+                     #se = "bootstrap", bootstrap = 10000)
+  write_rds(model_simple_hvr.fit, fname)
+  semTable(model_simple_hvr.fit, print.results = FALSE,
+           paramSets  = c("slopes", "constructed", "fits"),
+           columns    = c("estsestars", "z"),
+           fits       = c("npar", "chisq", "pvalue", "cfi", "tli",
+                          "aic", "bic2", "rmsea"),
+           type       = "html",
+           file       = here("data/derivatives/mediation_simple_hvr.html"))
+} else {
+  model_simple_hvr.fit <- read_rds(fname)
+}
+rm(fname)
 
-# Remove variances data
-estimates.dt  <- estimates.dt[op != "~~"]
-
-# Adjust for multiple comparisons
-estimates.dt[, pvalue_adj := p.adjust(pvalue, method = "bonferroni")]
-
-estimates.dt[, lhs_roi_id := as.numeric(str_extract(lhs, "\\d{3}"))]
-estimates.dt[, rhs_roi_id := as.numeric(str_extract(rhs, "\\d{3}"))]
+# MoCA mediation
+fname <- here("data/rds/mediation_rois_simple_moca.rds")
+if (!file.exists(fname) | refit_models) {
+  model_simple_moca.fit <- sem(moca.mod,
+                               data = triad_sel.dt,
+                               #group = "DX",
+                               estimator = "ML")
+                     #se = "bootstrap", bootstrap = 10000)
+  write_rds(model_simple_moca.fit, fname)
+  semTable(model_simple_moca.fit, print.results = FALSE,
+           paramSets  = c("slopes", "constructed", "fits"),
+           columns    = c("estsestars", "z"),
+           fits       = c("npar", "chisq", "pvalue", "cfi", "tli",
+                          "aic", "bic2", "rmsea"),
+           type       = "html",
+           file       = here("data/derivatives/mediation_simple_moca.html"))
+} else {
+  model_simple_moca.fit <- read_rds(fname)
+}
+rm(fname)
